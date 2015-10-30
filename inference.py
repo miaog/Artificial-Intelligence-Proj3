6735e50875f96b70834ac7f51d715a17061c7fdf
Gloria Miao <miaog@berkeley.edu>
2015-10-30 05:34:10 +0000

    8

--- inference.py
+++ inference.py
@@ -434,19 +434,23 @@ class JointParticleFilter(ParticleFilter):
         #   Get all the permutations
         prod = itertools.product(self.legalPositions)
         numberGhosts = self.numGhosts
-        i = 0
+        i = 1
         while i < numberGhosts:
             prod = itertools.product(self.legalPositions, prod)
+            i += 1
 
         #   Add the permutations to the list
         self.particles = []
         total = self.numParticles
     
         j = 0
+
+        prod1 = list(prod)
         while j < total:
-            for p in prod:
+            prod2 = prod1[:]
+            for p in prod2:
                 self.particles.append(p)
-                j += 1
+                j += 1 
     
         return random.shuffle(self.particles)
 
